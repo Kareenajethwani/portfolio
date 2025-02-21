2e5164c1199f1b648dc8ffbdd703c5e14fa67d38
kareena jethwani <missjethwani>
2025-02-21 23:45:38 +0530

    navbar changes

--- app.js
+++ app.js
@@ -43,9 +43,8 @@ function toggleServices() {
     }
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
+    const menuIcon = document.querySelector(".menu-icon");
     const navLinks = document.querySelector(".nav-links");
 
     menuIcon.addEventListener("click", function () {
